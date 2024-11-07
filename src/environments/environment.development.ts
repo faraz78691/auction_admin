23f11a5392772784822504821ba601759d437d04
faraz78691 <mohdfaraz.github@gmail.com>
2024-11-07 20:22:03 +0530

    chat fixed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
       production: true,
-      // baseUrl: 'http://98.80.36.64:5000/'
-        baseUrl: 'http://localhost:3000/'
+      baseUrl: 'http://98.80.36.64:5000/'
+        // baseUrl: 'http://localhost:5000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
       production: false,
       // baseUrl: 'http://98.80.36.64:5000/'
-      baseUrl: 'http://localhost:3000/'
+      baseUrl: 'http://localhost:5000/'
 };
