9cb65e441b52ee308381d879730c8e3828144369
Gagan-996 <mishragagan99@gmail.com>
2024-11-20 13:02:55 +0530

    dashboard data API added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  data: any;
+  loading: boolean = false;
 
+  constructor(public service: SharedService, private toastr: ToastrService,) {
+
+  }
+
+  ngOnInit() {
+    this.getDashboardData()
+  };
+
+  getDashboardData() {
+    let apiUrl = 'admin/dashboard'
+    this.service.get(apiUrl).subscribe((res: any) => {
+      if (res.success) {
+        this.data = res.data
+      } else {
+        this.toastr.error(res.message)
+      }
+    }, (err: any) => {
+      this.toastr.error(err)
+    })
+  };
 }
